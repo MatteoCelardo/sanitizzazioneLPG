59358abe6ec0d1ec1806eefc0179c86229685784
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-18 15:24:41 +0200

    creazione progetto

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace sanitizzazioneLPG;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*string test = Properties.Resources.SchemaJson;
+        StreamReader file = File.OpenText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"/schemaCatene.json");
+        JsonTextReader reader = new JsonTextReader(file);
+            
+        JSchema schema = JSchema.Load(reader); */
+
+    }
+}
