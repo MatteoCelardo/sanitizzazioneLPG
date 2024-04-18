132538aceab339256f135cd40669bbabaf5fa50f
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-18 15:26:14 +0200

    creazione delle classi del dominio

---
+++ Dominio/Catena.cs
@@ -0,0 +1,13 @@
+﻿namespace sanitizzazioneLPG;
+
+public class Catena :IDom
+{
+    private string[] _cat;
+
+    public Catena(string[] cat)
+    {
+        _cat = cat;
+    }
+
+    public string[] Cat {get => _cat; set => _cat = value; }
+}
---
+++ Dominio/Nodo/Nodo.cs
@@ -0,0 +1,23 @@
+﻿namespace sanitizzazioneLPG;
+
+public class Nodo : IDom
+{
+    private string _idCat;
+    private IdNodo_ _idNodo;
+    private bool _nodoSens;
+    private DaSanitizzareNodo_ _daSanitizzare;
+
+    public Nodo(string idCat, IdNodo_ idNodo, bool nodoSens, DaSanitizzareNodo_ daSanitizzare)
+    {
+        _idCat = idCat;
+        _idNodo = idNodo;
+        _nodoSens = nodoSens;
+        _daSanitizzare = daSanitizzare;
+    }
+
+    public string IdCat { get => _idCat; set => _idCat = value; }
+    public IdNodo_ IdNodo { get => _idNodo; set => _idNodo = value; }
+    public bool NodoSens {  get => _nodoSens; set => _nodoSens = value; }
+    public DaSanitizzareNodo_ DaSanitizzare { get => _daSanitizzare; set => _daSanitizzare = value; }
+
+}
