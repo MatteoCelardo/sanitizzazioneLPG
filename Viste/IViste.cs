c27dc29f6fc25404cc389e6b2fbaaede6b640cb1
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-19 17:14:37 +0200

    definizione dell'interfaccia delle viste

--- Viste/IViste.cs
+++ Viste/IViste.cs
@@ -2,5 +2,23 @@
 
 public interface IViste
 {
-
+    /// <summary>
+    /// richiede l’importazione del file JSON che contiene la specifica degli elementi 
+    /// da sanitizzare nel DB
+    /// </summary>
+    void importaJSON();
+    /// <summary>
+    /// richiede la cancellazione delle informazioni relative al file JSON caricato 
+    /// in precedenza con la funzione <c>importaJSON()</c>
+    /// </summary>
+    void cancellaJSON();
+    /// <summary>
+    /// richiede la validazione di un file JSON prima che venga importato
+    /// </summary>
+    void validaJSON();
+    /// <summary>
+    /// richiede la sanitizzazione del DB secondo le specifiche date dal file JSON 
+    /// importato con <c>importaJSON()</c>
+    /// </summary>
+    void sanitizzaDB();
 }
