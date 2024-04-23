c55c941c8e9bd71d9cd4c579fc1cf33ae012c524
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-23 15:01:55 +0200

    Ricreazione del progetto
    ricreazione del progetto secondo pattern MVVM interoducento avalonia UI.
    I vecchi sorgenti sono posizionati nella cartella olducento avalonia UI.

---
+++ sanitizzazioneLPG/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace sanitizzazioneLPG.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
