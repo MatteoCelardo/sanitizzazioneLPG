cc05123b6063eea75f4ae02ec707e9f7526e067a
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-24 22:38:50 +0200

    Modifica classi del dominio
    - inserito in Nodo e Relazione un costruttore vuoto per permettere al
      parser JSON di funzinare correttamente
    - tutti gli attributi di Nodo e Relazione messi a nullable a causa del
      costruttore vuoto

--- sanitizzazioneLPG/Dominio/Nodo/Nodo.cs
+++ sanitizzazioneLPG/Dominio/Nodo/Nodo.cs
@@ -3,9 +3,9 @@
 public class Nodo : IDom
 {
     private string? _idCat;
-    private IdNodo_ _idNodo;
-    private bool _nodoSens;
-    private DaSanitizzareNodo_ _daSanitizzare;
+    private IdNodo_? _idNodo;
+    private bool? _nodoSens;
+    private DaSanitizzareNodo_? _daSanitizzare;
 
     public Nodo(string idCat, IdNodo_ idNodo, bool nodoSens, DaSanitizzareNodo_ daSanitizzare)
     {
@@ -22,9 +22,13 @@ public class Nodo : IDom
         _daSanitizzare = daSanitizzare;
     }
 
+    public Nodo()
+    {
+    }
+
     public string? IdCat { get => _idCat; set => _idCat = value; }
-    public IdNodo_ IdNodo { get => _idNodo; set => _idNodo = value; }
-    public bool NodoSens {  get => _nodoSens; set => _nodoSens = value; }
-    public DaSanitizzareNodo_ DaSanitizzare { get => _daSanitizzare; set => _daSanitizzare = value; }
+    public IdNodo_? IdNodo { get => _idNodo; set => _idNodo = value; }
+    public bool? NodoSens {  get => _nodoSens; set => _nodoSens = value; }
+    public DaSanitizzareNodo_? DaSanitizzare { get => _daSanitizzare; set => _daSanitizzare = value; }
 
 }
--- sanitizzazioneLPG/Dominio/Relazione/Relazione.cs
+++ sanitizzazioneLPG/Dominio/Relazione/Relazione.cs
@@ -3,9 +3,9 @@
 public class Relazione : IDom
 {
     private string? _idCat; 
-    private IdRel_ _idRel;
-    private bool _relSens;
-    private DaSanitizzareRel_ _daSanitizzare;
+    private IdRel_? _idRel;
+    private bool? _relSens;
+    private DaSanitizzareRel_? _daSanitizzare;
 
     public Relazione(string idCat, IdRel_ idRel, bool relSens, DaSanitizzareRel_ ds)
     {
@@ -22,9 +22,13 @@ public class Relazione : IDom
         _daSanitizzare = ds;
     } 
 
+    public Relazione()
+    {
+    }
+
     public string? IdCat { get => _idCat ; set => _idCat = value; }
-    public IdRel_ IdRel { get => _idRel; set => _idRel = value; }
-    public bool RelSens { get => _relSens; set => _relSens = value; }
-    public DaSanitizzareRel_ DaSanitizzare { get => _daSanitizzare; set => _daSanitizzare = value; }
+    public IdRel_? IdRel { get => _idRel; set => _idRel = value; }
+    public bool? RelSens { get => _relSens; set => _relSens = value; }
+    public DaSanitizzareRel_? DaSanitizzare { get => _daSanitizzare; set => _daSanitizzare = value; }
 
 }
