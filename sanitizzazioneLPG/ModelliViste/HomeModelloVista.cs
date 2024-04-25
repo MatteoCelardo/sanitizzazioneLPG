3d3cea6a8492e34b845fde81e12c5d574aa0ccb0
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-25 11:16:40 +0200

    Terminazione logica HomeVista
    - Logica di funzionamento della vista HomeVista terminata
    - Parte grafica ancora da sistemare

--- sanitizzazioneLPG/ModelliViste/HomeModelloVista.cs
+++ sanitizzazioneLPG/ModelliViste/HomeModelloVista.cs
@@ -7,8 +7,6 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using MsBox.Avalonia;
-using MsBox.Avalonia.Enums;
 using sanitizzazioneLPG.Servizio;
 
 namespace sanitizzazioneLPG.ModelliViste;
@@ -47,7 +45,7 @@ public partial class HomeModelloVista : ObservableObject
     private async Task ImportaJSON()
     {
         // Get top level from the current control. Alternatively, you can use Window reference instead.
-        var topLevel = TopLevel.GetTopLevel(((IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow);
+        TopLevel topLevel = TopLevel.GetTopLevel(((IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow);
 
         // Start async operation to open the dialog.
         var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
@@ -61,16 +59,12 @@ public partial class HomeModelloVista : ObservableObject
         {
             Percorso = files[0].Path.AbsolutePath;
             _s.ImportaJSON(Percorso);
-            // Open reading stream from the first file.
             await using var stream = await files[0].OpenReadAsync();
-            using var streamReader = new StreamReader(stream);
-            // Reads all the content of file as a text.
-            //Console.Write(await streamReader.ReadToEndAsync());
+            StreamReader streamReader = new StreamReader(stream);
+            CodiceJson = streamReader.ReadToEnd();
         }
         else 
             _s.MostraMsg("Errore","Scegliere un file JSON da importare");
-            
-        
     }
 
     // funzione per specificare al file picker che deve prendere JSON
@@ -85,6 +79,8 @@ public partial class HomeModelloVista : ObservableObject
     private void CancellaJSON()
     {
         _s.CancellaJSON();
+        Percorso = null;
+        CodiceJson = null;
     }
 
     [RelayCommand(CanExecute = nameof(JsonPresente))]
