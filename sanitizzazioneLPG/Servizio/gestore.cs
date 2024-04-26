feb715a4d17f68b96a818ffa1f512b6a4dc535e1
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-26 15:05:26 +0200

    push del gestore per back up. sanitizzazione non implementata

--- sanitizzazioneLPG/Servizio/gestore.cs
+++ sanitizzazioneLPG/Servizio/gestore.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
 using Neo4jClient;
+using Neo4jClient.Cypher;
 using Newtonsoft.Json.Schema;
+using sanitizzazioneLPG.Dominio;
 using sanitizzazioneLPG.Persistenza;
 
 namespace sanitizzazioneLPG.Servizio;
@@ -71,31 +74,26 @@ public class Gestore : IServizio
 
     public async void SanitizzaDB(EnumSanit s)
     {
-        /*
-        var client = new BoltGraphClient("bolt://localhost:7687", "neo4j", "tesiCelardo2024");
-        await client.ConnectAsync();
-        
-        await client.Cypher
-                    .Create("(:User {name:'temp'}), (:User {name:'wer', casa:'dasdioa'})")
-                    .ExecuteWithoutResultsAsync();
-
-        await client.Cypher
-                    .Match("(u:User)")
-                    .Where("u.name = 'wer'")
-                    .Delete("u")
-                    .ExecuteWithoutResultsAsync();*/
-        Task<List<string>> queryNodi;
-        Task<List<string>> queryRel;
-        Task<List<string>> queryCat;
+        Task queryNodi;
+        Task queryRel;
+        Task queryCat;
 
         switch(s)
         {
             case EnumSanit.CANC: 
-                queryRel = Task.Run(GeneraQueryNodi);
-                queryNodi = Task.Run(GeneraQueryRel);
-                queryCat = Task.Run(GeneraQueryCat);
+                BoltGraphClient? client = new BoltGraphClient("bolt://localhost:7687", "neo4j", "tesiCelardo2024");
+                await client.ConnectAsync();
+
+                // generazione ed esecuzione di tutte le query in thread paralleli.
+                // Si ricorda che, stando alla documentazione e da piccoli test che 
+                // ho condotto, ConnectAsync() restituisce una connessione thread safe
+
+                queryNodi = Task.Run(() => this.SanitizzaNodi(client, _pers.ListAll(EnumTipoDom.NODI)));
+                queryRel = Task.Run(() => this.SanitizzaRel(client, _pers.ListAll(EnumTipoDom.RELAZIONI)));
+                queryCat = Task.Run(() => this.SanitizzaCat(client, _pers.ListAll(EnumTipoDom.CATENE)));
                 Task.WaitAll(queryRel, queryNodi, queryCat);
-                Console.WriteLine(queryRel.Result[0] + queryCat.Result[0] + queryNodi.Result[0]);
+
+                this.MostraMsg("Info", "Sanitizzazione portata a termine correttamente", Icon.Info, ButtonEnum.Ok);
                 break;
             default: 
                 this.MostraMsg("Errore", "Il tipo di sanitizzazione selezionato non esiste",Icon.Error,ButtonEnum.Ok);
@@ -116,28 +114,125 @@ public class Gestore : IServizio
         }
         return ret;
     }
-
+    
+    /// <summary>
+    /// permette di mostrare una finestra di popup contenente un messaggio
+    /// </summary>
+    /// <param name="titolo">titolo della finestra</param>
+    /// <param name="msg">messaggio da mostrare</param>
+    /// <param name="i">icona da mostrare</param>
+    /// <param name="b">bottoni da mostrare</param>
     public void MostraMsg(string titolo, string msg,Icon i = Icon.None, ButtonEnum b = ButtonEnum.Ok)
     {
         MessageBoxManager.GetMessageBoxStandard(titolo, msg, b, i).ShowAsync();
     }
 
 
-    private List<string> GeneraQueryRel()
+    private Task SanitizzaRel(BoltGraphClient bgc, List<IDom> lr)
     {
-        //await Task.Delay(TimeSpan.FromSeconds(7));
-        return ["prova"];
+        ICypherFluentQuery query;
+        
+        foreach(Relazione r in lr)
+        {
+            query = bgc.Cypher
+                .Match("(r)")
+                .WhereIf(!string.IsNullOrEmpty(r.IdRel.Etichetta),"$etichetta in labels(r)")
+                .WithParam("etichetta",r.IdRel?.Etichetta);
+
+            // aggiunta dei parametri in propNum
+            foreach(string k in r.IdRel.PropNum.Keys)
+            {
+                query = query.AndWhere("$key = $value").WithParams(new{key = k, value = r.IdRel.PropNum[k]});
+            }
+
+            // aggiunta dei parametri in propStr
+            foreach(string k in r.IdRel.PropStr.Keys)
+            {
+                query = query.AndWhere("$key = $value").WithParams(new{key = k, value = r.IdRel.PropStr[k]});
+            }
+
+
+            if(r.RelSens)
+                query = query.Delete("r");  // relazione interamente sensibile
+            else 
+            {
+                // relazione sensibile solo in parte
+                query.se
+            }
+
+            query.ExecuteWithoutResultsAsync();
+        }
+
+        /*
+        var stringsToRemove = new List<string> { "string1", "string2", "string3" };
+
+        var query = client.Cypher
+            .Match("(n:Node)")
+            .ForEach("(string IN $stringsToRemove | SET n.property = REPLACE(n.property, string, ''))")
+            .WithParam("stringsToRemove", stringsToRemove);
+        
+        query.ExecuteWithoutResults();
+        */
+
+        /*bgc.Cypher
+            .Create("(:User {name:'temp'}), (:User {name:'wer', casa:'dasdioa'})")
+            .ExecuteWithoutResultsAsync();
+
+        var query = bgc.Cypher
+                    .Match("(u)");
+        query = query.Where("$etichetta in labels(u)").WithParam("etichetta","User");
+        query = query.AndWhere("u.name = 'wer'");
+
+        query.Delete("u").ExecuteWithoutResultsAsync();   
+        return Task.Delay(2);     
+        
+        
+        await client.Cypher
+                    .Create("(:User {name:'temp'}), (:User {name:'wer', casa:'dasdioa'})")
+                    .ExecuteWithoutResultsAsync();
+
+        await client.Cypher
+                    .Match("(u:User)")
+                    .Where("u.name = 'wer'")
+                    .Delete("u")
+                    .ExecuteWithoutResultsAsync();*/
     }
 
-    private async Task<List<string>> GeneraQueryNodi()
+    private Task SanitizzaNodi(BoltGraphClient bgc, List<IDom> ln)
     {
-        await Task.Delay(TimeSpan.FromSeconds(4));
-        return ["ciao"];
+        return Task.Delay(1);
+        /*
+        bgc.Cypher
+            .Create("(:User {name:'nod1'})")
+            .ExecuteWithoutResultsAsync();
+        bgc.Cypher
+            .Create("(:User {name:'nod2'})")
+            .ExecuteWithoutResultsAsync();
+        bgc.Cypher
+            .Create("(:User {name:'nod3'})")
+            .ExecuteWithoutResultsAsync();
+        return bgc.Cypher
+            .Create("(:User {name:'nod4'})")
+            .ExecuteWithoutResultsAsync();
+            */
     }
 
-    private List<string> GeneraQueryCat()
+    private Task SanitizzaCat(BoltGraphClient bgc, List<IDom> lc)
     {
-        //await Task.Delay(TimeSpan.FromSeconds(3));
-        return ["casa"];
+        return Task.Delay(1);
+        /*
+        bgc.Cypher
+            .Create("(:User {name:'cat1'})")
+            .ExecuteWithoutResultsAsync();
+        bgc.Cypher
+            .Create("(:User {name:'cat2'})")
+            .ExecuteWithoutResultsAsync();
+        bgc.Cypher
+            .Create("(:User {name:'cat3'})")
+            .ExecuteWithoutResultsAsync();
+        return bgc.Cypher
+            .Create("(:User {name:'cat4'})")
+            .ExecuteWithoutResultsAsync();
+            */
     }
 }
