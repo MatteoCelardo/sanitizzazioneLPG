350969633faad580df42e92c280d01883d8e7d02
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-26 14:39:18 +0200

    rinominazione del file _idRel in IdRel_

---
+++ sanitizzazioneLPG/Dominio/Relazione/IdRel_.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace sanitizzazioneLPG.Dominio;
+
+public class IdRel_
+{
+    private string _etichetta; 
+    private IDictionary<string, string> _propStr;
+    private IDictionary<string, double> _propNum;
+
+    public IdRel_ (string etichetta, IDictionary<string, string> propStr, IDictionary<string, double> propNum) 
+    {
+        _etichetta = etichetta;
+        _propNum = propNum;
+        _propStr = propStr;
+    }
+
+    public string Etichetta { get => _etichetta; set => _etichetta = value; }
+    public IDictionary<string, string> PropStr {get => _propStr; set => _propStr = value;}
+    public IDictionary<string, double> PropNum {get => _propNum; set => _propNum = value;}
+}
