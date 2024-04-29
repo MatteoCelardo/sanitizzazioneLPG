fdd59af8fa4fe2b3afef599d0dc2ab51484f27b4
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-29 20:47:05 +0200

    aggiunta classi per la sanitizzazione

---
+++ sanitizzazioneLPG/Sanitizzazione/ISanit.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using sanitizzazioneLPG.Dominio;
+
+namespace sanitizzazioneLPG.Sanitizzazione;
+
+public interface ISanit
+{
+    // lista dei caratteri speciali da sanitizzare
+    protected static readonly IList<string> carEsc = new ReadOnlyCollection<string>(
+            new List<string>(["\'","\"","\\","/*","//"])
+        );
+    // lista delle parole chiave da sanitizzare
+    protected static readonly IList<string> parChiave = new ReadOnlyCollection<string>(
+            new List<string>(["RETURN", "AS", "LOAD", "OR", "AND", "CALL", "WITH"])
+        );
+
+    /// // <summary>
+    /// Sanitizza tutti i campi dell'oggetto <c>DaSanitizzareRel_</c> e degli oggetti 
+    /// che contiene
+    /// </summary>
+    /// <param name="dsr">Oggetto da sanitizzare</param>
+    /// <returns>Oggetto sanitizzato</returns>
+    static abstract DaSanitizzareRel_ SanitizzaRel(DaSanitizzareRel_ dsr); 
+
+    /// <summary>
+    /// Sanitizza tutti i campi dell'oggetto <c>DaSanitizzareNodo_</c> e degli oggetti 
+    /// che contiene
+    /// </summary>
+    /// <param name="dsn">Oggetto da sanitizzare</param>
+    /// <returns>Oggetto sanitizzato</returns>
+    static abstract DaSanitizzareNodo_ SanitizzaNodo(DaSanitizzareNodo_ dsn);
+}
---
+++ sanitizzazioneLPG/Sanitizzazione/Sanitizzatore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using sanitizzazioneLPG.Dominio;
+
+namespace sanitizzazioneLPG.Sanitizzazione;
+
+public class Sanitizzatore : ISanit
+{
+    
+    public static DaSanitizzareNodo_ SanitizzaNodo(DaSanitizzareNodo_ dsn)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static DaSanitizzareRel_ SanitizzaRel(DaSanitizzareRel_ dsr)
+    {
+        throw new NotImplementedException();
+    }
+}
