bec02e297e38d3cafd5a209647751cb337385cde
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-04-29 20:57:38 +0200

    spostamento liste costanti dall'interfaccia alla classe concreta

--- sanitizzazioneLPG/Sanitizzazione/ISanit.cs
+++ sanitizzazioneLPG/Sanitizzazione/ISanit.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using sanitizzazioneLPG.Dominio;
+﻿using sanitizzazioneLPG.Dominio;
 
 namespace sanitizzazioneLPG.Sanitizzazione;
 
 public interface ISanit
 {
-    // lista dei caratteri speciali da sanitizzare
-    protected static readonly IList<string> carEsc = new ReadOnlyCollection<string>(
-            new List<string>(["\'","\"","\\","/*","//"])
-        );
-    // lista delle parole chiave da sanitizzare
-    protected static readonly IList<string> parChiave = new ReadOnlyCollection<string>(
-            new List<string>(["RETURN", "AS", "LOAD", "OR", "AND", "CALL", "WITH"])
-        );
-
-    /// // <summary>
+    /// <summary>
     /// Sanitizza tutti i campi dell'oggetto <c>DaSanitizzareRel_</c> e degli oggetti 
     /// che contiene
     /// </summary>
--- sanitizzazioneLPG/Sanitizzazione/Sanitizzatore.cs
+++ sanitizzazioneLPG/Sanitizzazione/Sanitizzatore.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using sanitizzazioneLPG.Dominio;
 
 namespace sanitizzazioneLPG.Sanitizzazione;
 
 public class Sanitizzatore : ISanit
 {
-    
+    // lista dei caratteri speciali da sanitizzare
+    private static readonly IList<string> carEsc = new ReadOnlyCollection<string>(
+            new List<string>(["\'","\"","\\","/*","//"])
+        );
+    // lista delle parole chiave da sanitizzare
+    private static readonly IList<string> parChiave = new ReadOnlyCollection<string>(
+            new List<string>(["RETURN", "AS", "LOAD", "OR", "AND", "CALL", "WITH"])
+        );
+        
     public static DaSanitizzareNodo_ SanitizzaNodo(DaSanitizzareNodo_ dsn)
     {
         throw new NotImplementedException();
