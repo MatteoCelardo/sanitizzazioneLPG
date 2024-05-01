8e210bbe61b203375a458c706fe3e8b369635dbe
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-05-01 21:24:54 +0200

    Terminazione gestore
    per quanto riguarda i task generati dalla funzione di sanitizzazione del
    db:
    - correzione della condizione imposta per verificare nel match che la
      stringa presa dal JSON corrisponda all'etichetta di una relazione
    - correzione della sanitizzazione di propSensAssoc: prima non era
      rispettata la specifica siccome la rimozione non era condizionata dal
    booleano e dalle presenza degli elementi in propAssoc.

--- sanitizzazioneLPG/Servizio/gestore.cs
+++ sanitizzazioneLPG/Servizio/gestore.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MsBox.Avalonia;
@@ -9,7 +9,6 @@ using Neo4jClient.Cypher;
 using Newtonsoft.Json.Schema;
 using sanitizzazioneLPG.Dominio;
 using sanitizzazioneLPG.Persistenza;
-using sanitizzazioneLPG.Sanitizzazione;
 
 namespace sanitizzazioneLPG.Servizio;
 
@@ -141,7 +140,7 @@ public class Gestore : IServizio
             idr = r.IdRel;
             query = bgc.Cypher
                 .Match("()-[r]->()")
-                .WhereIf(!string.IsNullOrEmpty(idr.Etichetta),"$etichetta in labels(r)")
+                .WhereIf(!string.IsNullOrEmpty(idr.Etichetta),"type(r) = $etichetta")
                 .WithParam("etichetta",idr.Etichetta);
             
             query = this.CreaWhereProp(query, idr.PropStr, idr.PropNum,"r");
@@ -193,12 +192,15 @@ public class Gestore : IServizio
     private void SanitizzaCat(BoltGraphClient bgc, List<IDom> lc)
     {
         ICypherFluentQuery query;
-        // sintrga con la clausola match
-        string match;
-        // stringa con la clausola where
-        string where;
-        // stringa con la clausola remove
-        string remove;
+        // lista di stringhe contenenti clausole match. match[0] contiene il primo match
+        // mentr gli altri match[i] sono usati insieme alla stringa with per creare ulteriori 
+        // condizioni di rimozione per PropSensAssoc 
+        List<string> match = new List<string>();
+        // lista di stringhe con le clausole where. funzionamento analogo a match
+        List<string> where = new List<string>();
+        // lista di stringhe con la clausola remove. funzionamento analogo a where e match
+        List<string> remove = new List<string>();
+        string with; 
         // stringa con la clausola delete
         string delete;
 
@@ -230,24 +232,25 @@ public class Gestore : IServizio
             for(int j = 0; j < lc.Count; j++)
             {
                 c = (Catena) lc[j];
-                where = "";
-                remove = "";
+                where.Add("");
+                remove.Add("");
                 delete = "";
-                match = "(";
+                match.Add("(");
+                with = "";
 
                 for(int i = 0; i < c.Els.Count; i++)
                 {
                     if(c.Els[i].GetType() == typeof(Relazione))
                     {
                         r = (Relazione)c.Els[i];
-                        match += ")-[r" + j + i + "]->(";
+                        match[0] += ")-[r" + j + i + "]->(";
 
                         if(!string.IsNullOrEmpty(r.IdRel.Etichetta))
                         {
-                            if(string.IsNullOrEmpty(where))
-                                where = "$etichetta" + j + i + " in labels(r"+ j + i + ")";
+                            if(string.IsNullOrEmpty(where[0]))
+                                where[0] = "type(r"+ j + i + ") = $etichetta" + j + i;
                             else 
-                                where += " AND $etichetta" + j + i + " in labels(r"+ j + i + ")";
+                                where[0] += " AND type(r"+ j + i + ") = $etichetta" + j + i;
                             parEticStr.Add("etichetta" + j + i);
                             parValStr.Add(r.IdRel.Etichetta);
                         }
@@ -257,19 +260,19 @@ public class Gestore : IServizio
                         if(r.RelSens)
                             delete += "r" + j + i + ", ";
                         else if(r.DaSanitizzare != null)
-                            remove += this.CreaRemovePropSensCatena(r.DaSanitizzare.PropSempreSens, r.DaSanitizzare.PropSensAssoc,"r"+j+i);
+                            this.CreaRemovePropSensCatena(ref match,ref with, ref where, ref remove, r.DaSanitizzare.PropSempreSens, r.DaSanitizzare.PropSensAssoc,"r"+j+i);
                     }
                     else 
                     {
                         n = (Nodo)c.Els[i];
-                        match += "n" + j + i;
+                        match[0] += "n" + j + i;
 
                         if(n.IdNodo.Etichette.Length > 0)
                         {
-                            if(string.IsNullOrEmpty(where))
-                                where = "ALL(etic IN $etichette" + j+ i + " WHERE etic IN labels(n" + j + i +"))";
+                            if(string.IsNullOrEmpty(where[0]))
+                                where[0] = "ALL(etic IN $etichette" + j+ i + " WHERE etic IN labels(n" + j + i +"))";
                             else 
-                                where += " AND ALL(etic IN $etichette" + j +  i + " WHERE etic IN labels(n" + j + i +"))";
+                                where[0] += " AND ALL(etic IN $etichette" + j +  i + " WHERE etic IN labels(n" + j + i +"))";
                             parNodi.Add("etichette" + j + i);
                             parValNodi.Add(n.IdNodo.Etichette);
                         }
@@ -282,26 +285,31 @@ public class Gestore : IServizio
                         {
                             // rimozione di tutte le etichette sempre sensibili 
                             foreach(string e in n.DaSanitizzare.EtichetteSens)
-                                remove += "n" + j + i + ":" + e + ", ";
-                            remove += this.CreaRemovePropSensCatena(n.DaSanitizzare.PropSempreSens, n.DaSanitizzare.PropSensAssoc,"n"+j+i);
+                                remove[0] += "n" + j + i + ":" + e + ", ";
+                            this.CreaRemovePropSensCatena(ref match,ref with, ref where, ref remove, n.DaSanitizzare.PropSempreSens, n.DaSanitizzare.PropSensAssoc,"n"+j+i);
                         }
                     }
 
                 }
 
-                match += ")";
+                match[0] += ")";
 
                 query = bgc.Cypher
-                    .Match(match)
-                    .Where(where);
+                    .Match(match[0])
+                    .Where(where[0]);
 
                 if(!string.IsNullOrEmpty(delete))    
                     // rimuovo la virgola infondo alla stringa delete
                     query = query.DetachDelete(delete.Remove(delete.Length - 2));
 
-                if(!string.IsNullOrEmpty(remove))
+                if(!string.IsNullOrEmpty(remove[0]))
                     // rimuovo la virgola infondo alla stringa remove
-                    query = query.Remove(remove.Remove(remove.Length-2));
+                    query = query.Remove(remove[0].Remove(remove[0].Length-2));
+
+                // rimozione dei duplicati
+                with = string.Join(" ", with.Split(new Char[] {' '}).Distinct());
+                for(int k = 1; k < match.Count; k++)
+                    query = query.With(with.Remove(with.Length - 2)).Match(match[k]).Where(where[k]).Remove(remove[k]);
 
                 // inserimento dei parametri usati per le etichette dei nodi 
                 for(int i = 0; i < parNodi.Count; i++)
@@ -316,6 +324,10 @@ public class Gestore : IServizio
                     query = query.WithParam(parEticNum[i],parValNum[i]); 
 
                 query.ExecuteWithoutResultsAsync();
+
+                where.Clear();
+                remove.Clear();
+                match.Clear();
             }
         }
     }
@@ -392,21 +404,45 @@ public class Gestore : IServizio
     /// </returns>
     private ICypherFluentQuery CreaRemovePropSens(ICypherFluentQuery query, string[] propSempreSens, IDictionary<string,PropSensAssoc_> propSensAssoc, string nomeElem)
     {
+        // stringa usata come parametro per il match da inserire nel caso in cui 
+        // propSensAssoc contenga qualcosa
+        string match;
+
+        // inserimento di un match per un nodo o per una relazione in base al nome 
+        // dell'elemento passato
+        if(nomeElem.Contains("n"))
+            match = "(" + nomeElem + ")";
+        else 
+            match = "()-[" + nomeElem + "]->()";
+
         // rimozione di tutte le proprietà sempre sensibili
         foreach(string s in propSempreSens)
             query = query.Remove(nomeElem + "." + s);
                 
+        
         foreach(string p in propSensAssoc.Keys)
+        {
+            query = query.With(nomeElem);
+            query = query.Match(match);
             if(propSensAssoc[p].SanitizzareProp)
+            {
+                query = query.Where(nomeElem + "." + propSensAssoc[p].PropAssoc[0] + " IS NOT NULL");
+                for(int i = 1; i < propSensAssoc[p].PropAssoc.Length; i++)
+                    query = query.OrWhere(nomeElem + "." + propSensAssoc[p].PropAssoc[0] + " IS NOT NULL");
                 // se sanitizzare prop è a true, sanitizzo la proprietà in p, 
                 // ovvero quella che è resa sensibile dal vettore di proprietà 
                 // associate
                 query = query.Remove(nomeElem + "." + p);
+            }
             else 
-                // in caso contrario, sanitizzo tutte le proprietà associate
+            {
+                // in caso contrario, sanitizzo tutte le proprietà associate se p è 
+                // presente
+                query = query.Where(nomeElem + "." + p + " IS NOT NULL");
                 foreach (string pAssoc in propSensAssoc[p].PropAssoc)
                     query = query.Remove(nomeElem + "." + pAssoc);
-
+            }
+        }
         return query;
     }
 
@@ -418,7 +454,7 @@ public class Gestore : IServizio
     /// il parametro formale <c>where</c> conterrà la stringa aggiornata con le condizioni 
     /// specificare negli oggetti
     /// </summary>
-    /// <param name="where">parte di clausola where già scritta</param>
+    /// <param name="where">vettore con le clausole where scritte in precedenza</param>
     /// <param name="propStr">proprietà PropStr di <c>IdRel</c> o <c>IdNodo</c></param>
     /// <param name="propNum">proprietà PropNum di <c>IdRel</c> o <c>IdNodo</c></param>
     /// <param name="parEticStr">lista usata per tenere traccia delle etichette usate come parametro</param>
@@ -426,7 +462,7 @@ public class Gestore : IServizio
     /// <param name="parEticNum">analogo di <c>parEticStr</c> per le etichette cui corrisponde un valore numerico</param>
     /// <param name="parValNum">analogo di <c>parValStr</c> che contiene i valori numerici relativi a <c>parEticNum</c></param>
     /// <param name="nomeElem">nome dato nella clausola match all'elemento di cui si vogliono rimuovere le informazioni sensibili</param>
-    private void CreaWherePropCatena(ref string where, IDictionary<string,string> propStr, IDictionary<string,double> propNum, ref List<string> parEticStr, ref List<string> parValStr, ref List<string> parEticNum, ref List<double> parValNum, string nomeElem)
+    private void CreaWherePropCatena(ref List<string> where, IDictionary<string,string> propStr, IDictionary<string,double> propNum, ref List<string> parEticStr, ref List<string> parValStr, ref List<string> parEticNum, ref List<double> parValNum, string nomeElem)
     {
         IList<string> chiavi;
         int flag;
@@ -437,9 +473,9 @@ public class Gestore : IServizio
             // inserito a parte o meno. flag = 0 se non inserito a parte, 1 altrimenti
             flag = 0;
             //inserimento degli elementi in propNum
-            if(string.IsNullOrEmpty(where))
+            if(string.IsNullOrEmpty(where[0]))
             {
-                where = nomeElem + "." + chiavi[0] + " = $valuePN" + nomeElem + "0";
+                where[0] = nomeElem + "." + chiavi[0] + " = $valuePN" + nomeElem + "0";
                 parEticNum.Add("valuePN" + nomeElem + "0");
                 parValNum.Add(propNum[chiavi[0]]);
                 flag++;
@@ -447,7 +483,7 @@ public class Gestore : IServizio
             // tramite il flag, so se includere o meno il primo elemento nel ciclo
             for(int k = 0 + flag; k < chiavi.Count; k++)
             {
-                where += " AND " + nomeElem + "." + chiavi[k] + " = $valuePN" + nomeElem + k;
+                where[0] += " AND " + nomeElem + "." + chiavi[k] + " = $valuePN" + nomeElem + k;
                 parEticNum.Add("valuePN" + nomeElem + k);
                 parValNum.Add(propNum[chiavi[k]]);
             }
@@ -459,9 +495,9 @@ public class Gestore : IServizio
             flag = 0;
 
             //inserimento degli elementi in propstr
-            if(string.IsNullOrEmpty(where))
+            if(string.IsNullOrEmpty(where[0]))
             {
-                where = nomeElem + "." + chiavi[0] + " = $valuePS" + nomeElem + "0";
+                where[0] = nomeElem + "." + chiavi[0] + " = $valuePS" + nomeElem + "0";
                 parEticStr.Add("valuePS" + nomeElem + "0");
                 parValStr.Add(propStr[chiavi[0]]);
                 flag++;
@@ -469,7 +505,7 @@ public class Gestore : IServizio
             // tramite il flag, so se includere o meno il primo elemento nel ciclo
             for(int k = 0 + flag; k < chiavi.Count; k++)
             {
-                where +=  " AND " + nomeElem + "." + chiavi[k] + " = $valuePS" + nomeElem + k;
+                where[0] +=  " AND " + nomeElem + "." + chiavi[k] + " = $valuePS" + nomeElem + k;
                 parEticStr.Add("valuePS" + nomeElem + k);
                 parValStr.Add(propStr[chiavi[k]]);
             }
@@ -483,34 +519,56 @@ public class Gestore : IServizio
     /// <c>DaSanitizzareRel</c> o <c>DaSanitizzareNodo</c> per gli elementi del 
     /// dominio nelle catene
     /// </summary>
-    /// <param name="remove">parte di clausola remove già composta in precedenza</param>
+    /// <param name="match">vettore con le clausole remove scritte in precedenza</param>
+    /// <param name="with">stringa con la parte di clausola with scritta in precedenza</param>
+    /// <param name="where">vettore con le clausole remove scritte in precedenza</param>
+    /// <param name="remove">vettore con le clausole remove scritte in precedenza</param>
     /// <param name="propSempreSens">proprietà <c>PropSempreSens</c> di <c>DaSanitizzareRel</c> o <c>DaSanitizzareNodo</c></param>
     /// <param name="propSensAssoc">proprietà <c>PropSensAssoc</c> di <c>DaSanitizzareRel</c> o <c>DaSanitizzareNodo</c></param>
     /// <param name="nomeElem">nome dato nella clausola match all'elemento di cui si vogliono rimuovere le informazioni sensibili</param>
-    /// <returns>
-    /// ritorna la stringa <c>remove</c> aggioranta con le condizioni per rispettare
-    /// quanto specificato in <c>propSempreSens</c> e in <c>propSensAssoc</c>
-    /// </returns>
-    private string CreaRemovePropSensCatena(string[] propSempreSens, IDictionary<string,PropSensAssoc_> propSensAssoc, string nomeElem)
+    private void CreaRemovePropSensCatena(ref List<string> match, ref string with, ref List<string> where, ref List<string> remove,string[] propSempreSens, IDictionary<string,PropSensAssoc_> propSensAssoc, string nomeElem)
     {
-        string remove = "";
+        string stringaMatch;
+
+        // inserimento di un match per un nodo o per una relazione in base al nome 
+        // dell'elemento passato
+        if(nomeElem.Contains("n"))
+            stringaMatch = "(" + nomeElem + ")";
+        else 
+            stringaMatch = "()-[" + nomeElem + "]->()";
         
         // rimozione di tutte le proprietà sempre sensibili
         foreach(string s in propSempreSens)
-            remove += nomeElem + "." + s + ", ";
-                
+            remove[0] += nomeElem + "." + s + ", ";
+        
+
         foreach(string p in propSensAssoc.Keys)
+        {
+            with += nomeElem + ", ";
+            match.Add(stringaMatch);
             if(propSensAssoc[p].SanitizzareProp)
+            {
+                where.Add(nomeElem + "." + propSensAssoc[p].PropAssoc[0] + " IS NOT NULL");
+                for(int i = 1; i < propSensAssoc[p].PropAssoc.Length; i++)
+                    where[where.Count - 1 ] += " OR " + nomeElem + "." + propSensAssoc[p].PropAssoc[i] + " IS NOT NULL";
                 // se sanitizzare prop è a true, sanitizzo la proprietà in p, 
                 // ovvero quella che è resa sensibile dal vettore di proprietà 
                 // associate
-                remove += nomeElem + "." + p + ", ";
+                remove.Add(nomeElem + "." + p);
+            }
             else 
-                // in caso contrario, sanitizzo tutte le proprietà associate
+            {
+                // in caso contrario, sanitizzo tutte le proprietà associate se p è 
+                // presente
+                where.Add(nomeElem + "." + p + " IS NOT NULL");
+                remove.Add("");
                 foreach (string pAssoc in propSensAssoc[p].PropAssoc)
-                    remove += nomeElem + "." + pAssoc + ", ";
+                    remove[remove.Count - 1] += nomeElem + "." + pAssoc + ", ";
+                // rimozione di ", " dal fondo della stringa
+                remove[remove.Count - 1] = remove[remove.Count - 1].Remove(remove[remove.Count - 1].Length - 2);
+            }
+        }
 
-        return remove;
     }
     #endregion
 
