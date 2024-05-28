7ce7792551703dd7d22a5aad8b591e9b4e57888f
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-05-28 21:34:14 +0200

    Creazione dell'interfaccia di scelta del DB
    Creazione dell'interfaccia che permette di scegliere il DB che si vuole
    sanitizzare. Sono stati creati anche il file con gli stili e il
    view model ad essa relativi.

---
+++ sanitizzazioneLPG/ModelliViste/SceltaDBModelloVista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using sanitizzazioneLPG.Servizio;
+
+namespace sanitizzazioneLPG.ModelliViste;
+
+public class SceltaDBModelloVista : ObservableObject
+{
+    private readonly IServizio _s;
+
+    public SceltaDBModelloVista(IServizio s)
+    {
+        _s = s;
+    }
+
+    public SceltaDBModelloVista()
+    {
+        _s = Gestore.Istanza;
+    }
+}
