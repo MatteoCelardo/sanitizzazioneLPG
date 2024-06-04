51137b053e4da64048cc2ebfa3449fb08ff0ff80
MatteoCelardo <matteo.celardo@studenti.unimi.it>
2024-06-04 12:31:13 +0200

    Modifica al percorso file mostrato nella GUI
    aggiornamento della finestra della GUI che mostra il percorso al file.
    Ora mostra il percorso senza errori con gli accenti

--- sanitizzazioneLPG/ModelliViste/HomeModelloVista.cs
+++ sanitizzazioneLPG/ModelliViste/HomeModelloVista.cs
@@ -66,7 +66,7 @@ public partial class HomeModelloVista : ModelloVistaBase
         // e si è selezionato un file json
         if (files.Count >= 1 && _s.ImportaJSON(files[0].Path.LocalPath))
         {
-            Percorso = "File:\n" + files[0].Path.AbsolutePath;
+            Percorso = "File:\n" + files[0].Path.LocalPath;
             await using var stream = await files[0].OpenReadAsync();
             StreamReader streamReader = new StreamReader(stream);
             CodiceJson = streamReader.ReadToEnd();
